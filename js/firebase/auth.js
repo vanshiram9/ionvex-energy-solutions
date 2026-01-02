3504c1ba51df16436efbbcf53728227189dbe664
vanshiram9 <98492359+vanshiram9@users.noreply.github.com>
2026-01-02 08:06:26 +0530

    Update auth.js

--- js/firebase/auth.js
+++ js/firebase/auth.js
@@ -1,9 +1,15 @@
 import { auth } from "./app.js";
 import {
-  signInWithEmailAndPassword
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 export async function loginUser(email, password) {
   const res = await signInWithEmailAndPassword(auth, email, password);
+  return res.user;
+}
+
+export async function registerUser(email, password) {
+  const res = await createUserWithEmailAndPassword(auth, email, password);
   return res.user;
 }
